e955c06bd70614af7f626054b7d5c18d7768dfb1
AnangHanafi <ananghanafi>
2018-10-23 02:07:46 +0700

    Initial commit
    Signed-off-by: AnangHanafi <ananghanafi>

--- app/src/main/java/com/masbie/simon/MainActivity.java
+++ app/src/main/java/com/masbie/simon/MainActivity.java
@@ -3,31 +3,54 @@ package com.masbie.simon;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     private TextView mTextMessage;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
+            int id = item.getItemId();
+            System.out.println("id " + id);
+            switch (id) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    fragment = new HomeFragment();
+                    //    fragmentTransaction.replace(R.id.frameLayout, new HomeFragment()).commit();
+                    System.out.println("idhome " + id);
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    fragment = new SimonFragment();
+                    //    fragmentTransaction.replace(R.id.frameLayout, new PengaturanFragment()).commit();
+                    System.out.println("idprofile " + id);
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    fragment = new EncryptFragment();
+                    //       fragmentTransaction.replace(R.id.frameLayout, new InfoAplikasiFragment()).commit();
+                    System.out.println("idInfo " + id);
+                    break;
+                case R.id.navigation_dashboard2:
+                    fragment = new DecryptFragment();
+                    //       fragmentTransaction.replace(R.id.frameLayout, new InfoAplikasiFragment()).commit();
+                    System.out.println("idInfo " + id);
+                    break;
             }
-            return false;
+            final FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.frameLayout, fragment).commit();
+
+            return true;
         }
     };
 
@@ -35,10 +58,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        System.out.println("Tes ");
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, new HomeFragment()).commit();
+
+
+    }
 }
